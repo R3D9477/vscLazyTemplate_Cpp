d4568c76caf44696b01f935035935832abeb7015
R3D9477 <r3d9477@google.com>
2025-07-29 22:08:49 +0200

    main.cu cuda exec time

--- src/main.cu
+++ src/main.cu
@@ -14,14 +14,31 @@ __global__ void gridInfo()
 
 int main(void)
 {
-    gridInfo<<<2, 64>>>(); // kernel_name <<< num_of_blocks, num_of_threads_per_block >>> ()
-    cudaDeviceSynchronize();
+    {
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
 
-    CppInfo cppInfo;
-    std::cout << "C++ language version: " << cppInfo.GetLanguageVersion() << std::endl;
-    std::cout << "C++ compiler version: " << cppInfo.GetCompilerVersion() << std::endl;
+        cudaEventRecord(start);
 
-    std::cout << "Project version: " << project_version() << std::endl;
+        gridInfo<<<2, 64>>>();
+        cudaDeviceSynchronize();
+
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        float ms = 0;
+        cudaEventElapsedTime(&ms, start, stop);
+        std::cout << "CUDA kernel execution time: " << ms << "ms" << std::endl;
+    }
+
+    {
+        std::cout << "Project version: " << project_version() << std::endl;
+
+        CppInfo cppInfo;
+        std::cout << "C++ language version: " << cppInfo.GetLanguageVersion() << std::endl;
+        std::cout << "C++ compiler version: " << cppInfo.GetCompilerVersion() << std::endl;
+    }
 
     return 0;
 }
