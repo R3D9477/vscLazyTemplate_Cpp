e7bd3b53d01e4ee076425cb4f2795e3f954baeb1
R3D9477 <r3d9477@google.com>
2025-07-30 19:56:59 +0200

    filter

--- .vscode/nsighutils/generate_ncu_csv_report.py
+++ .vscode/nsighutils/generate_ncu_csv_report.py
@@ -5,7 +5,14 @@ import subprocess
 command = "ncu"
 report_path= "{0}.csv".format(sys.argv[2])
 
+report_lines = subprocess.check_output("ncu --metrics '{0}' --csv '{1}'".format(sys.argv[1], os.path.basename(sys.argv[2])), shell=True, text=True).splitlines()
+
 with open(report_path, 'w') as report_file:
-    report_file.write(subprocess.check_output("ncu --metrics '{0}' --csv '{1}'".format(sys.argv[1], sys.argv[2]), shell=True, text=True))
+    write_to_file = False
+    for line in report_lines:
+        if write_to_file:
+            report_file.write("%s\n" % line)
+        elif "==PROF== Disconnected" in line:
+            write_to_file = True
 
 os.system("code -r '{0}'".format(report_path))
