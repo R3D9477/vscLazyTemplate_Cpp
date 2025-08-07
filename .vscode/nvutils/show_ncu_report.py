8fd51d915d099c9009f951521b17f6262f66f56f
R3D9477 <r3d9477@google.com>
2025-08-07 15:14:24 +0200

    CUDA Pascal

--- .vscode/nvutils/show_ncu_report.py
+++ .vscode/nvutils/show_ncu_report.py
@@ -5,13 +5,14 @@ import subprocess
 command = "ncu"
 report_path= "{0}.csv".format(sys.argv[2])
 
-report_lines = subprocess.check_output("ncu --metrics '{0}' --csv '{1}'".format(sys.argv[1], sys.argv[2]), shell=True, text=True).splitlines()
+report_lines = subprocess.check_output(["ncu","--metrics", sys.argv[1], "--csv", sys.argv[2]]).decode().splitlines()
 
 with open(report_path, 'w') as report_file:
     write_to_file = False
     for line in report_lines:
         if write_to_file:
-            report_file.write("%s\n" % line)
+            if "==WARNING==" not in line:
+                report_file.write("%s\n" % line)
         elif "==PROF== Disconnected" in line:
             write_to_file = True
 
