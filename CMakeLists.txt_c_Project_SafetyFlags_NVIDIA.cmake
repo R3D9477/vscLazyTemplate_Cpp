035c02529639d1694f0736dc16f7c1092b0ce6aa
R3D9477 <r3d9477@google.com>
2025-08-08 17:33:33 +0200

    sf

--- CMakeLists.txt_c_Project_SafetyFlags_NVIDIA.cmake
+++ CMakeLists.txt_c_Project_SafetyFlags_NVIDIA.cmake
@@ -1,2 +1,13 @@
 # Safety flags for NVIDIA nvcc.
 # Some of the safety flags may request certain optimization level.
+
+list(APPEND SAFETY_FLAGS
+    --verbose
+    --display-error-number
+    --Werror=cross-execution-space-call
+    --Werror=reorder
+    --Werror=default-stream-launch
+    --Werror=ext-lambda-captures-this
+    #--Werror=deprecated-declarations
+    --device-stack-protector=true
+)
